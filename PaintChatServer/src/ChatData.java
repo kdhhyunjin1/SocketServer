610b9a605895c88e079b037d5042dd61409cdc58
kangjisung <kangjisung@naver.com>
2017-08-16 21:30:59 +0900

    final server

--- PaintChatServer/src/ChatData.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class ChatData implements Serializable {
-    String text;
-    public ChatData(String text){
-        this.text=text;
-    }
-}
---
+++ PaintChatServer/src/Protocol/PaintData.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.io.Serializable;
+
+public class PaintData implements Serializable {
+    int x;
+    int y;
+    int size;
+
+    public PaintData(int x, int y, int size){
+        this.x=x;
+        this.y=y;
+        this.size=size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
